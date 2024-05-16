325b786f9b864ac5334a4c6651c10ddb28b5eb65
hsuchenhsi <11056020@ntub.edu.tw>
2024-05-16 17:16:05 +0800

    0516 會員中心

--- home/views.py
+++ home/views.py
@@ -10,5 +10,14 @@ def login(request):
 def A01(request):
     return render(request,'A01.html')
 
+def describe(request):
+    return render(request,'describe.html')
+
 def A02(request):
     return render(request,'A02.html')
+
+def order(request):
+    return render(request,'order.html')
+
+def information(request):
+    return render(request,'information.html')
